411a870b893934f62623f7f7676eef8e178f8ba5
mebeZ <zacharyhine@gmail.com>
2023-10-30 15:09:16 -0400

    Update test decscriptions for loadImageFile

--- src/test/java/com/example/imaging/FaceControllerTest.java
+++ src/test/java/com/example/imaging/FaceControllerTest.java
@@ -34,12 +34,11 @@ class FaceControllerTest {
 	 * 1a. If we pass in an invalid file name - which is a non-empty string - we should get a FileNotFoundException
 	 * 1b. If we pass in an empty string, we should get a FileNotFoundException
 	 * 1c. If we pass in a null value, we should get a FileNotFoundException
-	 * 1d. If we pass in a valid file name, we should get a Mat object back, and the size of that Mat object should match the size of the image in static/images
-	 * 2. Mock image folder 
-	 * a. Multiple valid files - make sure only the first one is opened
-	 * b. No valid files is the same as case 1a - thus we can ignore this case
-	 * c. Only folders with no matching names 
-	 * d. Only folders with one matching name
+	 * 1d. If we pass in a valid file name, we should get a Mat object back, and the size of that Mat object should match the size of the image in static/images 
+	 * 2a. In a mock image folder there are multiple valid files. Make sure that only the first one is opened.
+	 * 2b. In a mock image folder there are no valid files. This is the same as case 1a. Thus we can ignore this case.
+	 * c. In a mock image folder there are only folders (no images), and none of the folders have names which match the parameter name. 
+	 * d. In a mock image folder there is one or more folders 
 	 * e. 
 	 */
 	@BeforeAll
@@ -82,9 +81,10 @@ class FaceControllerTest {
 	/**
 	 * Tests for getAvgIntensity method
 	 * 1a. A histogram (hist) with one or more negative values should throw an Exception because there cannot be negative pixel counts.
-	 * 1b. A histogram with all values equal to zero should throw an exception because the average brightness is in this case. 
-	 * 1c. A null histogram should throw an exception
-	 * 1d. A histogram which is an empty list should throw an exception
+	 * 1b. A histogram with all values equal to zero should throw an exception because the average brightness is undefined in this case. 
+	 * 1c. A histogram in which all non-zero values are outside of the range (i.e. hist = {0, 0, 0, 3, 3}, range = {0,2}) should throw an exception because the average brightness is undifined in this case.
+	 * 1d. A null histogram should throw an exception
+	 * 1e. A histogram which is an empty list should throw an exception
 	 * 2a. If range is incorrectly formatted with the start value below zero, then the method should throw an exception
 	 * 2b. If range is incorrectly formatted with the end value greater than hist.length - 1, then the method should throw an exception
 	 * 2c. If range is incorrectly formatted with both the start value and end value being invalid then the method should throw an exception. 
@@ -92,7 +92,8 @@ class FaceControllerTest {
 	 * 2e. If range has less than two elements, the method should throw an exception 
 	 * 2f. If range has more than two elements, the method should throw an exception
 	 * 3a. If hist only has one element, and the range is {0, 0}, then the return value should be 0 (i.e. the index of the one element of the hist)
-	 * 3b. If hist has more than one element
+	 * 3b. If hist only has one non-zero element and it is within the range, then the return value should be the index of that element
+	 * 3c. If hist has two or more non-zero elements, the return value should be the weighted average of the indices of those elements. For example, hist={2,8} and range={0,1} should return 0.8. 
 	 */
 
 }
