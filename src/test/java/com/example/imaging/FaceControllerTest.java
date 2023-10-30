329ce1cf5eebd735250fb7fe03d6f02c4652767b
wz2602 Wenchao Zhai <wz2602@columbia.edu>
2023-10-30 17:28:44 +0000

    add some tests for getAvgIntensity method

--- src/test/java/com/example/imaging/FaceControllerTest.java
+++ src/test/java/com/example/imaging/FaceControllerTest.java
@@ -1,5 +1,9 @@
 package com.example.imaging;
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
 
@@ -107,5 +111,111 @@ class FaceControllerTest {
 	 * 3a. If hist only has one element, and the range is {0, 0}, then the return value should be 0 (i.e. the index of the one element of the hist)
 	 * 3b. If hist has more than one element
 	 */
+
+
+	
+	//	 Tests for getAvgIntensity method
+	//	 1a. A histogram (hist) with one or more negative values should throw an Exception because there cannot be negative pixel counts.
+	@Test
+	void testNegativeValuesInHistogram() {
+		List<Double> hist = Arrays.asList(-1.0, 2.0, 3.0);
+		int[] range = {0, 2};
+		assertThrows(Exception.class, () -> FaceController.getAvgIntensity(hist, range));
+	}
+	
+	//	 1b. A histogram with all values equal to zero should throw an exception because the average brightness is in this case.
+	@Test
+	void testAllZeroHistogram() {
+		List<Double> hist = Arrays.asList(0.0, 0.0, 0.0);
+		int[] range = {0, 2};
+		assertThrows(Exception.class, () -> FaceController.getAvgIntensity(hist, range));
+	}
+	
+	//	 1c. A null histogram should throw an exception
+	@Test
+	void testNullHistogram() {
+		List<Double> hist = null;
+		int[] range = {0, 2};
+		assertThrows(Exception.class, () -> FaceController.getAvgIntensity(hist, range));
+	}
+	
+	
+	//	 1d. A histogram which is an empty list should throw an exception
+	@Test
+	void testEmptyHistogram() {
+		List<Double> hist = new ArrayList<>();
+		int[] range = {0, 2};
+		assertThrows(Exception.class, () -> FaceController.getAvgIntensity(hist, range));
+	}
+	
+	//	 2a. If range is incorrectly formatted with the start value below zero, then the method should throw an exception
+	@Test
+	void testInvalidStartValueInRange() {
+		List<Double> hist = Arrays.asList(1.0, 2.0, 3.0);
+		int[] range = {-1, 2};
+		assertThrows(Exception.class, () -> FaceController.getAvgIntensity(hist, range));
+	}
+	
+	//	 2b. If range is incorrectly formatted with the end value greater than hist.length - 1, then the method should throw an exception
+	@Test
+	void testInvalidEndValueInRange() {
+		List<Double> hist = Arrays.asList(1.0, 2.0, 3.0);
+		int[] range = {0, 3};
+		assertThrows(Exception.class, () -> FaceController.getAvgIntensity(hist, range));
+	}
+	
+	//	 2c. If range is incorrectly formatted with both the start value and end value being invalid then the method should throw an exception.
+	@Test
+	void testInvalidStartAndEndValuesInRange() {
+		List<Double> hist = Arrays.asList(1.0, 2.0, 3.0);
+		int[] range = {-1, 3};
+		assertThrows(Exception.class, () -> FaceController.getAvgIntensity(hist, range));
+	}
+	
+	//	 2d. If range is null, the method should through an exception
+	@Test
+	void testNullRange() {
+		List<Double> hist = Arrays.asList(1.0, 2.0, 3.0);
+		int[] range = null;
+		assertThrows(Exception.class, () -> FaceController.getAvgIntensity(hist, range));
+	}
+	
+	
+	//	 2e. If range has less than two elements, the method should throw an exception
+	@Test
+	void testRangeWithLessThanTwoElements() {
+		List<Double> hist = Arrays.asList(1.0, 2.0, 3.0);
+		int[] range = {0};
+		assertThrows(Exception.class, () -> FaceController.getAvgIntensity(hist, range));
+	}
+	
+	//	 2f. If range has more than two elements, the method should throw an exception
+	@Test
+	void testRangeWithMoreThanTwoElements() {
+		List<Double> hist = Arrays.asList(1.0, 2.0, 3.0);
+		int[] range = {0, 2, 3};
+		assertThrows(Exception.class, () -> FaceController.getAvgIntensity(hist, range));
+	}
+	
+	//	 3a. If hist only has one element, and the range is {0, 0}, then the return value should be 0 (i.e. the index of the one element of the hist)
+	@Test
+	void testSingleElementHistAndRangeZeroToZero() {
+		List<Double> hist = Arrays.asList(3.0);
+		int[] range = {0, 0};
+		double avgIntensity = FaceController.getAvgIntensity(hist, range);
+		assertEquals(0, avgIntensity);
+	}
+	
+	//	 3b. If hist has more than one element
+	@Test
+	void testMultipleElementHist() {
+		List<Double> hist = Arrays.asList(1.0, 2.0, 3.0, 4.0, 5.0);
+		int[] range = {1, 4};
+		// Average intensity calculation: (2*1 + 3*2 + 4*3 + 5*4) / (1 + 2 + 3 + 4)
+		double avgIntensity = FaceController.getAvgIntensity(hist, range);
+		assertEquals(3.0, avgIntensity, 0.001); // Using a delta for double comparison
+	}
+	//
+
 
 }
