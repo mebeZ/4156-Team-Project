3a5c869fc04ae0e52f0d4a24c650f6566b326ac3
WenchaoZhai <wz2602@columbia.edu>
2023-11-08 02:08:29 -0500

    refactor and add test cases for the method getImageName()

--- src/main/java/com/example/imaging/IOUtils.java
+++ src/main/java/com/example/imaging/IOUtils.java
@@ -4,6 +4,7 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.Paths;
 
 import javax.imageio.ImageIO;
 
@@ -12,42 +13,45 @@ import org.opencv.imgcodecs.Imgcodecs;
 
 public class IOUtils {
     /**
-	 * Locates a person's image file and load it into memory. If there are multiple valid image files containing a person's name, only the first one is loaded.
-	 * 
-	 * @param name is a person's name - must be a valid substring in at least one image file in resources/static/images
-	 * @exception FileNotFoundException if name does not appear in at least one image file 
-	 * @returns: On success, a matrix representing the loaded image file; on failure, a FileNotFoundException
-	 */
+     * Locates a person's image file and load it into memory. If there are multiple valid image files containing a person's name, only the first one is loaded.
+     *
+     * @param name is a person's name - must be a valid substring in at least one image file in resources/static/images
+     * @exception FileNotFoundException if name does not appear in at least one image file
+     * @returns: On success, a matrix representing the loaded image file; on failure, a FileNotFoundException
+     */
     // loadImageFile
-	public static String getPathToFile(String name) throws FileNotFoundException {
-		File imagesFolder = new File("src/main/resources/static/plain-images");
-		File[] imageFiles = imagesFolder.listFiles();
-		for (int i = 0; i < imageFiles.length; i++) {
-			if (imageFiles[i].isFile()) {
-				String filename = imageFiles[i].getName();
-				System.out.println(filename);
-				if (name == null || name.equals("")){
-					throw new FileNotFoundException("No image file containing name = " + name);
-				}
-				System.out.println(filename.indexOf(name));
-				if (filename.indexOf(name) >= 0) {
-					String filepath = imageFiles[i].getAbsolutePath();
+    public static String getPathToFile(String name) throws FileNotFoundException {
+        File imagesFolder = new File("src/main/resources/static/plain-images");
+        File[] imageFiles = imagesFolder.listFiles();
+        for (int i = 0; i < imageFiles.length; i++) {
+            if (imageFiles[i].isFile()) {
+                String filename = imageFiles[i].getName();
+                System.out.println(filename);
+                if (name == null || name.equals("")){
+                    throw new FileNotFoundException("No image file containing name = " + name);
+                }
+                System.out.println(filename.indexOf(name));
+                if (filename.indexOf(name) >= 0) {
+                    String filepath = imageFiles[i].getAbsolutePath();
                     return filepath;
                 }
-			}
-		}
-		throw new FileNotFoundException("No image file containing name = " + name);
-	}
+            }
+        }
+        throw new FileNotFoundException("No image file containing name = " + name);
+    }
 
     public static String getImageName(String name) {
         String fp = null;
         try {
             fp = getPathToFile(name);
         } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            System.exit(0);
+            // Rethrow as an IllegalArgumentException
+            throw new IllegalArgumentException("No image file containing name = " + name);
         }
-        return fp.substring(fp.lastIndexOf("/") + 1);
+
+        //return fp.substring(fp.lastIndexOf("\\") + 1);
+        // Use Paths.get(fp).getFileName() to extract only the filename without the path
+        return Paths.get(fp).getFileName().toString();
     }
 
     public static Mat loadFileAsMat(String name) {
--- src/test/java/com/example/imaging/IOUtilsTest.java
+++ src/test/java/com/example/imaging/IOUtilsTest.java
@@ -24,30 +24,30 @@ import javax.imageio.ImageIO;
 
 @SpringBootTest
 public class IOUtilsTest {
-    /*
+	/*
 	 * Tests for loadImageFile method
 	 * 1a. If we pass in an invalid file name - which is a non-empty string - we should get a FileNotFoundException
 	 * 1b. If we pass in an empty string, we should get a FileNotFoundException
 	 * 1c. If we pass in a null value, we should get a FileNotFoundException
-	 * 1d. If we pass in a valid file name, we should get a Mat object back, and the size of that Mat object should match the size of the image in static/images 
+	 * 1d. If we pass in a valid file name, we should get a Mat object back, and the size of that Mat object should match the size of the image in static/images
 	 * 2a. In a mock image folder there are multiple valid files. Make sure that only the first one is opened.
 	 * 2b. In a mock image folder there are no valid files. This is the same as case 1a. Thus we can ignore this case.
-	 * c. In a mock image folder there are only folders (no images), and none of the folders have names which match the parameter name. 
-	 * d. In a mock image folder there is one or more folders 
-	 * e. 
+	 * c. In a mock image folder there are only folders (no images), and none of the folders have names which match the parameter name.
+	 * d. In a mock image folder there is one or more folders
+	 * e.
 	 */
 
-    @BeforeAll
+	@BeforeAll
 	public static void loadLocally(){
 		OpenCV.loadLocally();
 	}
-     
-    @Test
+
+	@Test
 	void loadInvalidImageFile() {
 		assertThrows(FileNotFoundException.class, () -> IOUtils.getPathToFile("foo.txt")); // foo.txt is an invalid file
 	}
 
-	@Test 
+	@Test
 	void loadImageFromEmptyString() {
 		assertThrows(FileNotFoundException.class, () -> IOUtils.getPathToFile(""));
 	}
@@ -59,10 +59,10 @@ public class IOUtilsTest {
 
 	@Test
 	void loadValidImageFile() {
-        Mat result = IOUtils.loadFileAsMat("samantha");
-        Mat expected = Imgcodecs.imread( "src/main/resources/static/plain-images/samantha-green.jpeg");
-        Assertions.assertEquals(expected.rows(), result.rows());
-        Assertions.assertEquals(expected.cols(), result.cols());
+		Mat result = IOUtils.loadFileAsMat("samantha");
+		Mat expected = Imgcodecs.imread( "src/main/resources/static/plain-images/samantha-green.jpeg");
+		Assertions.assertEquals(expected.rows(), result.rows());
+		Assertions.assertEquals(expected.cols(), result.cols());
 	}
 
 	@Test
@@ -141,10 +141,93 @@ public class IOUtilsTest {
 		try {
 			ExpectedBufferedImage = ImageIO.read(file);
 		} catch (IOException e) {
-				e.printStackTrace();
-				System.exit(0);
+			e.printStackTrace();
+			System.exit(0);
 		}
 		Assertions.assertEquals(bufferedImagesEqual(ExpectedBufferedImage, result), true);
 	}
+
+
+	//Test cases of getFileName()
+	// a. If we pass in a valid file name, we should get the correct image name and check its correctness.
+	@Test
+	void testFileNameWithValidNameAndCheckCorrectness() {
+		String imageName = IOUtils.getImageName("carl");
+		Assertions.assertTrue(imageName.contains("carl"));
+		Assertions.assertTrue(imageName.endsWith(".jpeg"));
+	}
+
+	// b. If we pass in a non-existent name, we should get an IllegalArgumentException.
+	@Test
+	void testFileNameWithNonExistentName() {
+		assertThrows(IllegalArgumentException.class, () -> IOUtils.getImageName("nonexistentname"));
+	}
+
+	// c. If we pass in an empty string, we should get an IllegalArgumentException.
+	@Test
+	void testFileNameFromEmptyString() {
+		assertThrows(IllegalArgumentException.class, () -> IOUtils.getImageName(""));
+	}
+
+	// d. If we pass in a null value, we should get an IllegalArgumentException.
+	@Test
+	void testFileNameWithNull() {
+		assertThrows(IllegalArgumentException.class, () -> IOUtils.getImageName(null));
+	}
+
+	// e. If we pass in a valid name, we should get the correct image name.
+	@Test
+	void testFileNameWithValidName() {
+		String name = "samantha";
+		String expectedName = "samantha-green.jpeg";
+		String result = IOUtils.getImageName(name);
+
+		// Extract the file name from the full path
+		// resultFileName = Paths.get(result).getFileName().toString();
+
+		Assertions.assertEquals(expectedName, result);
+	}
+
+	// f. If we pass in a valid name, we should get the correct image name, and the associated image file should exist.
+	@Test
+	void testValidNameReturnsFile() {
+		String imageName = IOUtils.getImageName("samantha");
+		String imagePath = "src/main/resources/static/plain-images/" + imageName;
+		File imageFile = new File(imagePath);
+		Assertions.assertTrue(imageFile.exists());
+	}
+
+
+//	@Test
+//	void getImageNameMultipleValidFiles() {
+//		// Create a mock image folder with multiple valid files
+//		File mockImageFolder = new File("src/main/resources/static/images/mock-images");
+//		mockImageFolder.mkdirs();
+//
+//		// Copy valid image files to the mock folder
+//		Path sourcePath1 = Paths.get("src/main/resources/static/plain-images/samantha-green.jpeg");
+//		Path destinationPath1 = Paths.get("src/main/resources/static/images/mock-images/samantha-green.jpeg");
+//		try {
+//			Files.copy(sourcePath1, destinationPath1, StandardCopyOption.REPLACE_EXISTING);
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//
+//		Path sourcePath2 = Paths.get("src/main/resources/static/plain-images/carl-blue.jpeg");
+//		Path destinationPath2 = Paths.get("src/main/resources/static/images/mock-images/samantha-blue.jpeg");
+//		try {
+//			Files.copy(sourcePath2, destinationPath2, StandardCopyOption.REPLACE_EXISTING);
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//
+//
+//		String imageName = IOUtils.getImageName("samantha");
+//		Assertions.assertEquals("samantha-green.jpeg", imageName); // Verify that the first valid file is used
+//
+//		mockImageFolder.delete();
+//
+//	}
+
 
 }
