b79835c0dfd4ce58edd8959eac663721c53429c9
WenchaoZhai <wz2602@columbia.edu>
2023-11-09 17:11:09 -0500

    add tests for getPathToFile() and some comments

--- src/test/java/com/example/imaging/IOUtilsTest.java
+++ src/test/java/com/example/imaging/IOUtilsTest.java
@@ -25,7 +25,7 @@ import javax.imageio.ImageIO;
 @SpringBootTest
 public class IOUtilsTest {
 	/*
-	 * Tests for loadImageFile method
+	 * Tests for getPathToFile method
 	 * 1a. If we pass in an invalid file name - which is a non-empty string - we should get a FileNotFoundException
 	 * 1b. If we pass in an empty string, we should get a FileNotFoundException
 	 * 1c. If we pass in a null value, we should get a FileNotFoundException
@@ -64,6 +64,80 @@ public class IOUtilsTest {
 		Assertions.assertEquals(expected.rows(), result.rows());
 		Assertions.assertEquals(expected.cols(), result.cols());
 	}
+
+	// c. In a mock image folder there are only folders (no images), and none of the folders have names which match the parameter name.
+	@Test
+	void loadImageFromFolderWithOnlySubfoldersNoMatch() {
+		// Create subfolders without matching names
+		File subfolder1 = new File("src/main/resources/static/plain-images/subfolder1");
+		File subfolder2 = new File("src/main/resources/static/plain-images/subfolder2");
+		subfolder1.mkdirs();
+		subfolder2.mkdirs();
+
+		try {
+			assertThrows(FileNotFoundException.class, () -> IOUtils.getPathToFile("nonexistentname"));
+		} finally {
+			// Clean up created subfolders
+			subfolder1.delete();
+			subfolder2.delete();
+		}
+	}
+
+
+	// d. In a mock image folder there is one or more folders
+	@Test
+	void loadImageFromFolderWithSubfoldersAndImagesFirstMatchingSubfolder() {
+		File subfolder1 = new File("src/main/resources/static/plain-images/subfolder1");
+		File subfolder2 = new File("src/main/resources/static/plain-images/subfolder2");
+		subfolder1.mkdirs();
+		subfolder2.mkdirs();
+
+		// Create an image file in both subfolders
+		Path sourcePath1 = Paths.get("src/main/resources/static/plain-images/samantha-green.jpeg");
+		Path destinationPath1 = Paths.get("src/main/resources/static/plain-images/subfolder1/samantha-green.jpeg");
+		Path sourcePath2 = Paths.get("src/main/resources/static/plain-images/carl-blue.jpeg");
+		Path destinationPath2 = Paths.get("src/main/resources/static/plain-images/subfolder2/carl-blue.jpeg");
+
+		try {
+			Files.copy(sourcePath1, destinationPath1, StandardCopyOption.REPLACE_EXISTING);
+			Files.copy(sourcePath2, destinationPath2, StandardCopyOption.REPLACE_EXISTING);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		String imagePath = null;
+		try {
+			imagePath = IOUtils.getPathToFile("samantha");
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+
+		Assertions.assertNotNull(imagePath);
+		Assertions.assertTrue(imagePath.endsWith("samantha-green.jpeg"));
+
+		// Clean up: delete the created folders and files
+		deleteFolder(subfolder1);
+		deleteFolder(subfolder2);
+	}
+	// Helper method to delete the mock folders and files
+	private void deleteFolder(File folder) {
+		File[] files = folder.listFiles();
+		if (files != null) {
+			for (File file : files) {
+				if (file.isDirectory()) {
+					deleteFolder(file);
+				} else {
+					file.delete();
+				}
+			}
+		}
+		folder.delete();
+	} // end of unit tests for the getPathToFile()
+
+
+	/*
+	 * Start of unit tests for the loadFileAsMat() method in the IOUtils class.
+	 */
 
 	@Test
 	void multipleValidImageFile() {
@@ -145,10 +219,34 @@ public class IOUtilsTest {
 			System.exit(0);
 		}
 		Assertions.assertEquals(bufferedImagesEqual(ExpectedBufferedImage, result), true);
-	}
+	} // end of unit tests for the loadFileAsMat()
 
 
-	//Test cases of getFileName()
+	/*
+	 * Start of unit tests for the getImageName method in the IOUtils class.
+	 *
+	 * Test cases:
+	 * a. If we pass in a valid file name, we should get the correct image name and check its correctness.
+	 *    This test ensures that when a valid file name is provided, the resulting image name
+	 *    contains the specified name and ends with the ".jpeg" extension.
+	 *
+	 * b. If we pass in a non-existent name, we should get an IllegalArgumentException.
+	 *    This test checks if an IllegalArgumentException is thrown when a non-existent name is passed.
+	 *
+	 * c. If we pass in an empty string, we should get an IllegalArgumentException.
+	 *    This test verifies that an IllegalArgumentException is thrown when an empty string is used as input.
+	 *
+	 * d. If we pass in a null value, we should get an IllegalArgumentException.
+	 *    This test checks if the method throws an IllegalArgumentException when a null value is provided.
+	 *
+	 * e. If we pass in a valid name, we should get the correct image name.
+	 *    This test ensures that the correct image name is obtained when a valid name is passed.
+	 *
+	 * f. If we pass in a valid name, we should get the correct image name, and the associated image file should exist.
+	 *    This test checks both the correctness of the image name and the existence of the associated image file
+	 *    when a valid name is provided.
+	 */
+
 	// a. If we pass in a valid file name, we should get the correct image name and check its correctness.
 	@Test
 	void testFileNameWithValidNameAndCheckCorrectness() {
@@ -195,7 +293,7 @@ public class IOUtilsTest {
 		String imagePath = "src/main/resources/static/plain-images/" + imageName;
 		File imageFile = new File(imagePath);
 		Assertions.assertTrue(imageFile.exists());
-	}
+	} // end of unit tests for the getImageName()
 
 
 //	@Test
