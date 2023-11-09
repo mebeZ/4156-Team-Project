3afee09d9cbbe4407435133dcce610bfdb78b348
17PZ <145883663+17PZ@users.noreply.github.com>
2023-11-09 14:42:17 -0500

    Update PoseControllerTest.java

--- src/test/java/com/example/imaging/PoseControllerTest.java
+++ src/test/java/com/example/imaging/PoseControllerTest.java
@@ -1,8 +1,11 @@
 package com.example.imaging;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import org.opencv.core.*;
 import org.springframework.boot.test.context.SpringBootTest;
-
 import nu.pattern.OpenCV;
 
 @SpringBootTest
@@ -12,4 +15,18 @@ public class PoseControllerTest {
 		OpenCV.loadLocally();
 	}
     
+@Test
+    void testForwardFacingFace() throws Exception {
+        String imageName = "forward_facing.jpg"; // This image should be of a person facing forward
+        double yawAngle = PoseController.getYawAngle(imageName);
+        assertTrue(Math.abs(yawAngle) < 15.0, "Expected yaw angle to be less than 15 degrees for a forward-facing face");
+    }
+
+    // 3b. Test for a non-forward-facing face, yaw angle should be significant
+    @Test
+    void testNonForwardFacingFace() throws Exception {
+        String imageName = "profile_facing.jpg"; // This image should be of a person facing sideways
+        double yawAngle = PoseController.getYawAngle(imageName);
+        assertFalse(Math.abs(yawAngle) < 15.0, "Expected yaw angle to be greater than 15 degrees for a non-forward-facing face");
+    }
 }
