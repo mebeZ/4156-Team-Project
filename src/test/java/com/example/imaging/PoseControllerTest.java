9ea5015d29476c6b758269ec3acfd126ffdc62cf
17PZ <145883663+17PZ@users.noreply.github.com>
2023-11-09 15:07:01 -0500

    Update PoseControllerTest.java

--- src/test/java/com/example/imaging/PoseControllerTest.java
+++ src/test/java/com/example/imaging/PoseControllerTest.java
@@ -14,8 +14,21 @@ public class PoseControllerTest {
 	public static void loadLocally(){
 		OpenCV.loadLocally();
 	}
+    // 1a. If the image does not contain any faces, an exception should be thrown
+    @Test
+    void testNoFacesDetected() {
+        String imageName = "no_faces_image.jpg"; //  no faces
+        assertThrows(Exception.class, () -> PoseController.getYawAngle(imageName));
+    }
 
-    // 1a. Test for a forward-facing face, yaw angle should be near 0
+    // 1b. If the face landmarks cannot be detected, an exception should be thrown
+    @Test
+    void testNoFaceDescriptors() {
+        String imageName = "no_landmarks_image.jpg"; //  face  with undetectable landmarks
+        assertThrows(Exception.class, () -> PoseController.getYawAngle(imageName));
+    }
+
+    // 2a. Test for a forward-facing face, yaw angle should be near 0
     @Test
     void testForwardFacingFace() throws Exception {
         String imageName = "forward_facing.jpg"; //  facing forward
@@ -23,7 +36,7 @@ public class PoseControllerTest {
         assertTrue(Math.abs(yawAngle) < 15.0, "Expected yaw angle to be less than 15 degrees for a forward-facing face");
     }
 
-    // 1b. Test for a non-forward-facing face, yaw angle should be significant
+    // 2b. Test for a non-forward-facing face, yaw angle should be significant
     @Test
     void testNonForwardFacingFace() throws Exception {
         String imageName = "profile_facing.jpg"; //  facing sideways
