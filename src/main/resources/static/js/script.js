94fcc0b3eb0f48d491fb450f84a71e3709f54009
mebeZ <zacharyhine@gmail.com>
2023-12-11 15:48:01 -0500

    Minor changes

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,12 @@
+// https://developer.mozilla.org/en-US/docs/Web/API/Media_Capture_and_Streams_API/Taking_still_photos
+(() => {
+  const width = 320; // scale photo width to this
+  const height = 0; // Computed based on the input stream
+
+  const streaming = false;
+
+  let video = null;
+  let canvas = null;
+  let photo = null;
+  let startbutton = null;
+});
