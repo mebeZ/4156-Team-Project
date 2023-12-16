7e84fefd287214c0c2d75b62ffb384e8b0b2a4f2
mebeZ <zacharyhine@gmail.com>
2023-12-16 17:13:16 -0500

    Submit dropdown and get name of selected image

--- src/main/resources/static/js/dropdown.js
+++ src/main/resources/static/js/dropdown.js
@@ -1,5 +1,19 @@
+const submitButton = document.getElementById("submitButton");
+var selectedImageName = null;
+
+
+/*
+Modify the button text to match the clicked dropdown item @element
+*/
 function changeDropdownTitle(element) {
     var imageName = element.innerText;
     var button = document.getElementById("dropdownMenuButton");
     button.innerHTML = imageName;
+    selectedImageName = imageName;
 }
+
+submitButton.addEventListener('click', function() {
+    if (selectedImageName != null) {
+        console.log(selectedImageName);
+    }
+});
